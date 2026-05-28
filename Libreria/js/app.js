21948a0675298f0b43ded3c2ecaaf4b1d2890550
Berehm <estrellahuertaminaya@gmail.com>
2026-05-28 14:44:31 -0500

    bere

--- Libreria/js/app.js
+++ Libreria/js/app.js
@@ -1,15 +1,24 @@
 function login() {
-      const usuario  = document.getElementById('usuario').value.trim();
-      const password = document.getElementById('password').value.trim();
-      const errorMsg = document.getElementById('errorMsg');
 
-      if (usuario === 'admin' && password === '1234') {
+    const usuario  = document.getElementById('usuario').value.trim();
+    const password = document.getElementById('password').value.trim();
+    const errorMsg = document.getElementById('errorMsg');
+
+    if (usuario === 'admin' && password === '1234') {
+
+        // GUARDAR SESIÓN
+        localStorage.setItem("login", "true");
+
+        // IR AL DASHBOARD
         window.location.href = '../index.html';
-      } else {
-        errorMsg.classList.add('show');
-      }
-    }
 
-    document.addEventListener('keydown', e => {
-      if (e.key === 'Enter') login();
-    });
+    } else {
+
+        errorMsg.classList.add('show');
+
+    }
+}
+
+document.addEventListener('keydown', e => {
+    if (e.key === 'Enter') login();
+});
